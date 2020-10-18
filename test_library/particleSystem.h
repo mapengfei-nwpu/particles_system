b797d77950fc958300b7ede317ffec2b591e895f
Your Name <you@example.com>
2020-10-18 12:15:13 +0800

    测试生成的动态链接库是否有用

---
+++ test_library/particleSystem.h
@@ -0,0 +1,69 @@
+
+
+#ifndef __PARTICLESYSTEM_H__
+#define __PARTICLESYSTEM_H__
+
+#include "particles_kernel.cuh"
+#include "vector_functions.h"
+
+// Particle system class
+class ParticleSystem
+{
+    public:
+        // These parameters are enough to construct a background grid.
+        ParticleSystem(
+            uint numParticles, float cellRadius,
+            float xOrigin, float yOrigin, float zOrigin,
+            float xSize,   float ySize,   float zSize);
+        
+        // default background grid is [-1,-1,-1]x[1,1,1]
+        ParticleSystem(uint numParticles, float cellRadius):
+            ParticleSystem(numParticles, cellRadius, -1.0f, -1.0f, -1.0f, 2.0f, 2.0f, 2.0f)    
+        {
+            //
+        }
+        
+        // default background with cell raius 0.2. 
+        ParticleSystem(uint numParticles):
+            ParticleSystem(numParticles, 0.2)
+        {
+            //
+        }
+
+        void inputData(float* pos, float* val);
+        void interpolate(uint numParticleNew, float* pos, float* val);
+        ~ParticleSystem();
+
+    protected: // methods
+        ParticleSystem() {}
+
+        void _initialize(int numParticles);
+        void _finalize();
+
+    protected: // data
+        bool m_bInitialized;
+        uint m_numParticles;
+
+        // GPU data
+        float *m_dPos;
+        float *m_dVal;
+        float *m_dPos_new;
+        float *m_dVal_new;
+
+        float *m_dSortedPos;
+        float *m_dSortedVal;
+
+        // grid data for sorting method
+        uint  *m_dGridParticleHash; // grid hash value for each particle
+        uint  *m_dGridParticleIndex;// particle index for each particle
+        uint  *m_dCellStart;        // index of start of each cell in sorted list
+        uint  *m_dCellEnd;          // index of end of cell
+
+        // params
+        SimParams m_params;
+        uint3 m_gridSize;
+        uint m_numGridCells;
+
+};
+
+#endif // __PARTICLESYSTEM_H__
---
+++ test_library/particles.cpp
@@ -0,0 +1,80 @@
+// Includes
+#define NDEBUG
+#include <assert.h>
+#include <stdlib.h>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <time.h>
+
+#include "particleSystem.h"
+/// 一个奇怪的现象，如果 edge_divide 是 5 的倍数，那么最后的插值结果会比较精确。
+/// 这和粒子的坐标的是否准确有关。
+const uint edge_divide = 19;
+const uint num_old = edge_divide*edge_divide*edge_divide;
+const uint num_new = 600;
+const float radius = 0.2;
+
+void data_generate(std::vector<float> &pos_old,
+                   std::vector<float> &pos_new,
+                   std::vector<float> &val_old) {
+
+    for (size_t i = 0; i < edge_divide; i++)
+    {
+        for (size_t j = 0; j < edge_divide; j++)
+        {
+            for (size_t k = 0; k < edge_divide; k++)
+            {
+                auto n = i * edge_divide * edge_divide + j * edge_divide + k;
+                pos_old[n * 3    ] = i / static_cast<float>(edge_divide+1) + 0.5 / static_cast<float>(edge_divide+1)-0.5;
+                pos_old[n * 3 + 1] = j / static_cast<float>(edge_divide+1) + 0.5 / static_cast<float>(edge_divide+1)-0.5;
+                pos_old[n * 3 + 2] = k / static_cast<float>(edge_divide+1) + 0.5 / static_cast<float>(edge_divide+1)-0.5;
+            }
+        }
+    }
+
+    // grid positions and weights.
+    srand(static_cast<uint>(time(0)));
+    auto rrr = [] {return static_cast<float>(rand()) / static_cast<float>(RAND_MAX); };
+    for (size_t i = 0; i < val_old.size()/4; i++)
+    {
+        val_old[i * 4    ] = 4.0;
+        val_old[i * 4 + 1] = 3.0;
+        val_old[i * 4 + 2] = 2.0;
+        val_old[i * 4 + 3] = 8.0 / static_cast<float>((edge_divide + 1)*(edge_divide + 1)*(edge_divide + 1)) / (radius * radius * radius);
+    }
+
+    // random positions.
+    for (size_t i = 0; i < pos_new.size(); i++)
+    {
+        pos_new[i] = rrr()*0.3;
+    }
+}
+
+
+int main() {
+    std::vector<float> pos_old(3*num_old);
+    std::vector<float> val_old(4*num_old);
+    std::vector<float> pos_new(3*num_new);
+    std::vector<float> val_new(3*num_new);
+
+    data_generate(pos_old, pos_new, val_old);
+    
+    assert(pos_old.size() * 4 == val_old.size() * 3);
+    assert(pos_new.size() == val_new.size());
+
+    /// use the particle system for delta interpolation.
+    ParticleSystem particle_system(pos_old.size() / 3, radius);
+    particle_system.inputData(pos_old.data(), val_old.data());
+    particle_system.interpolate(pos_new.size() / 3, pos_new.data(), val_new.data());
+    
+    /// print the results.
+    for (size_t i = 0; i < pos_new.size() / 3; i++)
+    {
+        printf("pos: %f, %f, %f\n", pos_new[3 * i], pos_new[3 * i + 1], pos_new[3 * i + 2]);
+        printf("val: %f, %f, %f\n", val_new[3 * i], val_new[3 * i + 1], val_new[3 * i + 2]);
+    }
+    
+    // here, val_new have been rewritten.
+    return 0;
+}
